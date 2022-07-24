11b40da1cea1e9aa67faecfaf265b6c21391e35e
taurusilver7 <prasad20697@gmail.com>
2022-07-24 18:54:14 +0530

    main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.scss";
+import "swiper/swiper.min.css";
+import "./assets/boxicons-2.0.7/css/boxicons.min.css"
 import React from "react";
+import Header from "./components/header/Header";
+import { BrowserRouter, Route } from "react-router-dom";
+import Routes from "./config/Routes";
+import Footer from "./components/footer/Footer";
 function App() {
-  return <div>Hyperstream Movie streamning service.</div>;
+  return (
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          <>
+            <Header {...props} />
+            <Routes />
+            <Footer />
+          </>
+        )}
+      />
+    </BrowserRouter>
+  );
 }
 
 export default App;
