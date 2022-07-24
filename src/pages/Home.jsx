074721ffb7621ba0f33b07952db1a30139d758e9
taurusilver7 <prasad20697@gmail.com>
2022-07-24 23:44:09 +0530

    home page completed save footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { category, movieType, tvType } from "../api/tmdbApi";
 import { OutlineButton } from "../components/button/Button";
 import HeroSlide from "../components/hero-slide/HeroSlide";
+import MovieList from "../components/movieList/MovieList";
 
 const Home = () => {
   return (
@@ -11,9 +13,41 @@ const Home = () => {
         <div className="section mb-3">
           <div className="section__header mb-2">
             <h2>Trending Movies</h2>
-            <Link to="/movie"></Link>
-            <OutlineButton className="small">View more</OutlineButton>
+            <Link to="/movie">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
           </div>
+          <MovieList category={category.movie} type={movieType.popular} />
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated Movies</h2>
+            <Link to="/movie">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.movie} type={movieType.top_rated} />
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending TV</h2>
+            <Link to="/tv">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.popular} />
+        </div>
+
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Top Rated TV</h2>
+            <Link to="/tv">
+              <OutlineButton className="small">View more</OutlineButton>
+            </Link>
+          </div>
+          <MovieList category={category.tv} type={tvType.top_rated} />
         </div>
       </div>
     </>
