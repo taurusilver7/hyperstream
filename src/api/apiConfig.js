a1fd4f8f897e2fc0bb108a8abee7c017a8480505
taurusilver7 <prasad20697@gmail.com>
2022-07-24 16:39:56 +0530

    api config file

---
+++ src/api/apiConfig.js
@@ -0,0 +1,7 @@
+const apiConfig = {
+  baseUrl: `https://api.themoviedb.org/3/`,
+  apiKey: "f74ccdd76a7932d4705499b7725a338c",
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
+export default apiConfig;
