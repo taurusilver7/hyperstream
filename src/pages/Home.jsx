96102f6066891b02bb51345168fce7aed77f342f
taurusilver7 <prasad20697@gmail.com>
2022-07-24 22:48:28 +0530

    home page init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { OutlineButton } from "../components/button/Button";
+import HeroSlide from "../components/hero-slide/HeroSlide";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <>
+      <HeroSlide />
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Trending Movies</h2>
+            <Link to="/movie"></Link>
+            <OutlineButton className="small">View more</OutlineButton>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
