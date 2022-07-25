e25ee13d4e46c8a3c8d70cbd20a96aca6138b5ad
taurusilver7 <prasad20697@gmail.com>
2022-07-25 15:35:26 +0530

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import "./footer.scss";
+import bg from "../../assets/footer-bg.jpg";
+import logo from "../../assets/tmovie.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer component</div>;
+  return (
+    <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+            <Link to="/">Hyperstream</Link>
+          </div>
+        </div>
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact Us</Link>
+            <Link to="/">Terms of Services</Link>
+            <Link to="/">About</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Privacy</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Must Watch</Link>
+            <Link to="/">Recent Releases</Link>
+            <Link to="/">Terms of Services</Link>
+            <Link to="/">Top on Hyperstream</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
