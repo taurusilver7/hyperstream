77d3bd441b465465465fb4dcaabb1a5586f81bdd
taurusilver7 <prasad20697@gmail.com>
2022-07-25 17:10:36 +0530

    detail page completed

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import apiConfig from "../../api/apiConfig";
 import tmdbApi from "../../api/tmdbApi";
+import MovieList from "../../components/movieList/MovieList";
+import CastList from "./CastList";
 import "./detail.scss";
+import VideoList from "./VideoList";
 
 const Detail = () => {
   const { catagory, id } = useParams();
@@ -13,7 +16,7 @@ const Detail = () => {
     const getDetail = async () => {
       const res = await tmdbApi.details(catagory, id, { params: {} });
       setItem(res);
-      console.log(res);
+      // console.log(res);
       window.scrollTo(0, 0);
     };
     getDetail();
@@ -42,7 +45,36 @@ const Detail = () => {
                 }}
               ></div>
             </div>
-            <div className="movie-content__info"></div>
+            <div className="movie-content__info">
+              <h1 className="title">{item.title || item.name}</h1>
+              <div className="genres">
+                {item.genres &&
+                  item.genres.slice(0, 4).map((gen, i) => (
+                    <span key={i} className="genres__item">
+                      {gen.name}
+                    </span>
+                  ))}
+              </div>
+              <p className="overview">{item.overview}</p>
+              <div className="cast">
+                <div className="section__header">
+                  <h2>Cast</h2>
+                </div>
+                {/* cast list */}
+                <CastList id={item.id} />
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="section mb-3">
+              <VideoList id={item.id} />
+            </div>
+            <div className="section mb-3">
+              <div className="section__header mb-2">
+                <h2>Similar</h2>
+              </div>
+              <MovieList category={catagory} type="similar" id={item.id} />
+            </div>
           </div>
         </>
       )}
