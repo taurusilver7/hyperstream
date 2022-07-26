fc4e2519fd10c48f7424ed83ea53a7a8ef10df3a
taurusilver7 <prasad20697@gmail.com>
2022-07-26 19:29:57 +0530

    api key hidden in env

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,9 +1,7 @@
 const apiConfig = {
   baseUrl: `https://api.themoviedb.org/3/`,
-  apiKey: "f74ccdd76a7932d4705499b7725a338c",
+  apiKey: process.env.API_KEY,
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
 export default apiConfig;
-
-
