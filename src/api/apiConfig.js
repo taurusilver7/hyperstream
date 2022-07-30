ac7a220fc98c8ab40da4829d47f22ead5dae578c
taurusilver7 <prasad20697@gmail.com>
2022-07-30 23:43:36 +0530

    api added from env

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: `https://api.themoviedb.org/3/`,
-  apiKey: process.env.API_KEY,
+  apiKey: "f74ccdd76a7932d4705499b7725a338c",
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
